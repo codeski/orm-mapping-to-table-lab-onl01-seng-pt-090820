3bf34f9c87a2d9d9fb15c32f783a7109125c7d84
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-05 21:39:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
   end
   
   
-  # def self.create_table
-  #   sql = <<-SQL CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT) SQL DB[:conn].execute(sql)
-  # end
+  def self.create_table
+    sql = <<-SQL CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT) SQL DB[:conn].execute(sql)
+  end
 end
