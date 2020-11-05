df67c36bd3022b3bfbc455176be68ef863f68de8
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-05 21:44:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,6 +17,8 @@ class Student
   
   
   def self.create_table
-    sql = <<-SQL CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT) SQL
-  end 
+    sql = <<-SQL CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
+    SQL
+    DB[:conn].execute(sql) 
+    end 
 end
